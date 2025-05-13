27d1f67a3ba30e41e329537f51ab6ea4ae2c4e1b
JosueTorresN <jtn1999@estudiantec.cr>
2025-05-13 16:38:05 -0600

    My first push

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port = 3000
+app.get("/",(req,res)=>{
+    res.send("hellow world")
+    })
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Proyect is running")
+    }
+})
