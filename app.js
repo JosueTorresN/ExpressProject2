7a4f8de9fd593bb7a67d5274231736e63ef3cd0b
JosueTorresN <119891985+JosueTorresN@users.noreply.github.com>
2025-05-14 17:26:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const port = 3000
 app.get("/",(req,res)=>{
-    res.send("hellow world 3")
+    res.send("hellow world 33")
     })
 app.listen(port,(error)=>{
     if(!error){
